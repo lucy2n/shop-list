ea290060f700c87b7e3f78e8068c2f4357edc5a6
lucy2n <lyusianaumenko@yandex.by>
2023-09-01 13:42:33 +0300

    feat: add InputCategoria

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,10 +2,11 @@ import { Main } from '../../pages/main/main';
 import React from 'react';
 
 import { InputQuantity } from '../input-quantity/input-quantity';
+import { InputCategoria } from '../input-categoria/input-categoria';
 
 function App() {
   return (
-    <InputQuantity />
+    <InputCategoria />
   );
 }
 
